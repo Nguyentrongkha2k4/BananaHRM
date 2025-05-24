9cf5d6df7b70301e31935a13c9ce2d39d8fcbcb4
Nguyen Trong Kha <trongkha08022k4@gmail.com>
2025-05-24 23:33:04 +0700

    feat(jwtAuthentication): finish gen jwtToken

--- api/src/main/java/com/bananahrm/hrms/Component/jwtAuthentication.java
+++ api/src/main/java/com/bananahrm/hrms/Component/jwtAuthentication.java
@@ -1,7 +1,25 @@
 package com.bananahrm.hrms.Component;
 
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.bananahrm.hrms.Entity.User;
+import com.bananahrm.hrms.Exception.AppException;
+import com.bananahrm.hrms.Exception.ErrorCode;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSObject;
+import com.nimbusds.jose.Payload;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 
 
 @Component
@@ -9,5 +27,37 @@ public class jwtAuthentication {
     @Value("${jwt.secret_key}")
     private String SECRET_KEY;
     
+    @Value("${jwt.expirationtime}")
+    private long EXPIRATION_TIME;
+
+    public boolean checkPassword(String requestPassword, String hashPassword) throws Exception{
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(requestPassword, hashPassword);
+    }
+
+    public String generateToken(User user) throws Exception{
+        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS512);
+
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                                            .subject(user.getId().toString())
+                                            .issuer("dev")
+                                            .issueTime(new Date())
+                                            .expirationTime(
+                                                new Date(Instant.now().plus(EXPIRATION_TIME, ChronoUnit.SECONDS).toEpochMilli()))
+                                            .claim("scope", user.getRole())
+                                            .build();
+
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+        JWSObject jWSObject = new JWSObject(jwsHeader, payload);
+
+        jWSObject.sign(new MACSigner(SECRET_KEY.getBytes()));
+
+        return jWSObject.serialize();
+    }
+
+    public SignedJWT parSignedJWT(String token) throws ParseException{
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        return signedJWT;
+    }
 
 }
--- api/src/main/java/com/bananahrm/hrms/Configuration/RedisConfig.java
+++ api/src/main/java/com/bananahrm/hrms/Configuration/RedisConfig.java
@@ -1,11 +1,7 @@
 package com.bananahrm.hrms.Configuration;
 
-import java.time.Duration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
