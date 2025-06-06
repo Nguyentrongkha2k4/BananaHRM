9a4a59e251c4b43bf661df7b80bf6e94d6b61381
Nguyentrongkha2k4 <trongkha08022k4@gmail.com>
2025-06-06 10:26:44 +0700

    fix(be): mapping leave request

--- api/src/main/java/com/bananahrm/hrms/mapper/LeaveRequestMapper.java
+++ api/src/main/java/com/bananahrm/hrms/mapper/LeaveRequestMapper.java
@@ -3,8 +3,12 @@ package com.bananahrm.hrms.mapper;
 import com.bananahrm.hrms.DTO.response.LeaveRequestResponse;
 import com.bananahrm.hrms.Entity.LeaveRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LeaveRequestMapper {
+    @Mapping(target = "updateBy", ignore = true)
+    @Mapping(target = "leaveType", ignore = true)
+    @Mapping(target = "maNhanVien", ignore = true)
     public LeaveRequestResponse toDTO(LeaveRequest leaveRequest);
 }
