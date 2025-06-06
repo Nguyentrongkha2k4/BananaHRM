d99b3df53950fb4e2570ab6d758eb1309998ac5f
Nguyentrongkha2k4 <trongkha08022k4@gmail.com>
2025-06-06 12:09:53 +0700

    fix(be): fix bug in mapping leave request

--- api/src/main/java/com/bananahrm/hrms/mapper/LeaveRequestMapper.java
+++ api/src/main/java/com/bananahrm/hrms/mapper/LeaveRequestMapper.java
@@ -2,13 +2,10 @@ package com.bananahrm.hrms.mapper;
 
 import com.bananahrm.hrms.DTO.response.LeaveRequestResponse;
 import com.bananahrm.hrms.Entity.LeaveRequest;
-import lombok.AccessLevel;
-import lombok.experimental.FieldDefaults;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-@FieldDefaults(level = AccessLevel.PUBLIC)
 public interface LeaveRequestMapper {
     @Mapping(target = "updateBy", ignore = true)
     @Mapping(target = "leaveType", ignore = true)
